23779f0c34713d2b93996da0acbfd99e17b061a1
NewApp <MSP>
2023-10-03 08:28:26 +0200

    Initial Commit

---
+++ Calc/CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcApp.swift
+//  Calc
+//
+//  Created by Maria Paszkowska on 03/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
